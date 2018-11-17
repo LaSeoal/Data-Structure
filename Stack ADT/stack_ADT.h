2457af2465c06c16d1235e95d61fb2e0fcba92f1
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2018-11-17 15:46:37 +0900

    Update stack_ADT.h

--- Stack ADT/stack_ADT.h
+++ Stack ADT/stack_ADT.h
@@ -1,16 +1,18 @@
 #include <stdbool.h>
 
+// Stack Node
 typedef struct node {
 	void* data;
 	struct node* link;
 } STACK_NODE;
 
+// STAcK
 typedef struct {
 	int count;
 	STACK_NODE* top;
 } STACK;
 
+// Operations
 STACK* create_stack();
-
 bool push( STACK* stack, void* in );
 void* pop( STACK* stack );
